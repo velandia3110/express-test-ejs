d4f1c7e40b734cb01f5b0aec7890419eff6a6346
Julian Velandia <julianvelandia3110@gmail.com>
2024-09-22 18:37:14 -0500

    crud functions added

--- router/pets.js
+++ router/pets.js
@@ -5,7 +5,7 @@ const Pet = require('../models/pet')
 
 router.get('/', async (req, res) =>{
     try{
-        const petsArray = await Pet.find({})
+        const petsArray = await Pet.find()
         console.log(petsArray)
 
         res.render("pets", {
@@ -16,6 +16,80 @@ router.get('/', async (req, res) =>{
     }
     
     
+})
+
+router.get('/create', (req, res) => {
+    res.render("create")
+})
+
+router.post('/', async (req, res) => {
+    const body = req.body
+    try{
+        await Pet.create(body)
+        res.redirect('/pets')
+    }catch(err){
+        console.error(err)  
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try{
+        const petDB = await Pet.findOne({_id: id})
+        res.render("detail", {
+            pet: petDB,
+            error: false
+        })
+
+    } catch(err){
+        console.error(err)
+        res.render("detail", {
+            error: true,
+            message: "No se encuentra el id deseado"
+        })
+    } 
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    try{
+        petDB = await Pet.findByIdAndDelete({_id: id})
+        if(petDB){
+            res.json({
+                state: true,
+                message: 'Eliminado'
+            })
+        }else{
+            res.json({
+                state: false,
+                message: 'Fallo al eliminar'
+            })
+        }
+    }catch(err){
+        console.error(err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    try{
+        const petDB = await Pet.findByIdAndUpdate(
+            id,
+            body,
+            {userFindAnnModify:false}
+        )
+        res.json({
+            state: true,
+            message: 'Actualizado'
+        })
+    }catch(err){
+        console.error(err)
+        res.json({
+            state: false,
+            message: 'Falla al actualizar'
+        })
+    }
 })
 
 module.exports = router
